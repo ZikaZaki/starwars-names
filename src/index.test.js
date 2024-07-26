b42beb935ad4ad6796bc7a9065167aa21bb5a9ca
ZikaZaki <17160061@su.edu.ye>
2024-07-26 03:05:40 +0300

    feat(random): add ability to get an array of starwars-names
    If you pass a number to the 'random' function, you will receive an array with that number of random
    items
    
    closes #6

--- src/index.test.js
+++ src/index.test.js
@@ -23,5 +23,13 @@ describe("starwars-names", function () {
       var randomItem = starwars.random();
       expect(starwars.all).to.include(randomItem);
     });
+
+    it("should return an array of random items if passed a number", function () {
+      var randomItems = starwars.random(3);
+      expect(randomItems).to.have.length(3);
+      randomItems.forEach(function (item) {
+        expect(starwars.all).to.include(item);
+      });
+    });
   });
 });
